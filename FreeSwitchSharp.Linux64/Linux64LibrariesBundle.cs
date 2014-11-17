01d5b403803605eeb5bbcd4f8f0944f085b9f6a7
Daniel Weber <dw@exram.de>
2014-11-17 20:02:19 +0100

    Add FreeSwitchSharp.Linux64-project files.
    Revert EOL.

---
+++ FreeSwitchSharp.Linux64/Linux64LibrariesBundle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace FreeSwitchSharp.Linux64
+{
+	using FreeSwitchSharp.Native;
+
+	public class Linux64LibrariesBundle : INativeLibraryBundle
+	{
+		private static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+		private static readonly string ResourcesPath = typeof(Linux64LibrariesBundle).Namespace + ".Libs.";
+		private static readonly List<INativeLibraryResource> _resources = new List<INativeLibraryResource>();
+
+		static Linux64LibrariesBundle()
+		{
+			foreach (var res in Assembly.GetManifestResourceNames())
+			{
+				if (res.StartsWith(ResourcesPath))
+				{
+					var fileName = res.Substring(ResourcesPath.Length);
+					_resources.Add(new EmbeddedNativeLibraryResource(Assembly, res, fileName));
+				}
+			}
+		}
+
+		#region INativeLibraryBundle Members
+
+		public bool SupportsCurrentPlatform
+		{
+			get
+			{
+				return Environment.OSVersion.Platform == PlatformID.Unix && NativeLibrariesManager.RunningIn64Bits;
+			}
+		}
+
+		public IEnumerable<INativeLibraryResource> Resources
+		{
+			get { return _resources; }
+		}
+
+		#endregion
+	}
+}
